035c72878cba5f3b6978905b96ae6510e82642a5
Sahil <ss6611786@gmail.com>
2023-07-20 23:16:19 +0530

    added middlewares

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,1 +1,13 @@
 const express = require('express');
+import { registerController } from '../controllers/authController';
+
+
+
+//rest Objects
+const routes = express.Router();
+
+
+// routing
+routes.post('/register', registerController, (req, res) => {
+
+})
