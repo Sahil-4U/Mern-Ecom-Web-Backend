4d8f4a05dec963520c5ba467b07bdd7f1e3d36be
Sahil <ss6611786@gmail.com>
2023-07-29 17:39:41 +0530

    add a new end point for dashboard page and name of end point /user-auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,9 @@ const router = express.Router();
 // requests
 router.post('/register', registerController);
 router.post('/login', loginController);
+router.get('/user-auth', (req, res) => {
+    return res.status(200).send("OK");
+})
 
 // export
 export default router;
