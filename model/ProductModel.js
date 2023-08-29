6d3eb642d77e83661a64c3aef0d9387afb46d8b0
Sahil <ss6611786@gmail.com>
2023-08-29 21:33:47 +0530

    so i have created a new schema for productspage and today i have learned for save image in db we have to write type buffer and contentType:String

---
+++ model/ProductModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: mongoose.ObjectId,
+        ref: 'category',
+        // ab hum yha pr reference create kr rhe h to humko us model ka jo export keyword h yani jo model ka name liya h vhi likhna h or mongoose.ObjectId ek type ka id hota h jo database khud se generate krta h:)
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    photo: {
+        type: Buffer,
+        contentType: String
+        // agar humko koi bhi image database m save krni ho to humko type:Buffer or contentType:String lena hi pdega or buffer m hum sirf 15 mb tk ki file save kr skte h
+
+    },
+    shipping: {
+        type: Boolean
+    }
+}, { timestamps: true })
+
+export default mongoose.model('productsSchema', productSchema);
