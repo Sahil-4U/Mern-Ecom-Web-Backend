3c852babe133f4d672efd2ae4ad603b17c90ae16
Sahil <ss6611786@gmail.com>
2023-08-30 23:31:56 +0530

    now

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { isAdmin, requireSingIn } from '../middlewares/authMiddleware.js';
 import { createProductController } from '../controller/productController.js';
+import formidable from 'express-formidable';
 
 const router = express.Router();
 
 
-router.post('/create-product', requireSingIn, isAdmin, createProductController);
+router.post('/create-product', requireSingIn, isAdmin, formidable(), createProductController);
 
 
 export default router;
