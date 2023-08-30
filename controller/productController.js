c76e6021a63acec503c14e71677f829408ecd379
Sahil <ss6611786@gmail.com>
2023-08-30 23:24:00 +0530

    we download a package for uploading images in backend package name is formidable

--- controller/productController.js
+++ controller/productController.js
@@ -1,3 +1,12 @@
 export const createProductController = async (req, res) => {
+    try {
 
+} catch (error) {
+    console.log(error);
+    return res.status(500).send({
+        success: false,
+        error,
+        message: 'Error in create Product Controller'
+    })
+}
 }
