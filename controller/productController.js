67a63d65e572b00bdf9592b65142c6296ace4e72
Sahil <ss6611786@gmail.com>
2023-08-31 22:57:02 +0530

    our update-product route works good

--- controller/productController.js
+++ controller/productController.js
@@ -41,7 +41,7 @@ export const createProductController = async (req, res) => {
         error,
         message: 'Error in create Product Controller'
     })
-}
+    }
 }
 
 // get all products
@@ -117,6 +117,48 @@ export const deleteproductController = async (req, res) => {
             success: false,
             message: 'error while delete product',
             error
+        })
+    }
+}
+
+// update product contoller
+export const updateProductController = async (req, res) => {
+    try {
+        const { name, slug, description, price, category, quantity, shipping } = req.fields;
+        const { photo } = req.files;
+
+        // now we check everything is find or not we implement here validations with the help of switch statement
+        switch (true) {
+            case !name:
+                return res.status(500).send({ error: 'Name is required' })
+            case !description:
+                return res.status(500).send({ error: 'Description is required' })
+            case !price:
+                return res.status(500).send({ error: 'Price is required' })
+            case !category:
+                return res.status(500).send({ error: 'Category is required' })
+            case !quantity:
+                return res.status(500).send({ error: 'Quantity is required' })
+            case photo && photo.size < 100000:
+                return res.status(500).send({ error: 'Photo  is required and size should be less than 1mb' })
+        }
+        const products = await ProductModel.findByIdAndUpdate(req.params.pid, { ...req.fields, slug: slugigy(name) }, { new: true });
+        if (photo) {
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.path;
+        }
+        await products.save();
+        return res.status(201).send({
+            success: true,
+            message: "products updated successfully",
+            products,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            error,
+            message: 'Error in update Product Controller'
         })
     }
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { isAdmin, requireSingIn } from '../middlewares/authMiddleware.js';
-import { createProductController, deleteproductController, getproductsController, getsingleproductController, photoController } from '../controller/productController.js';
+import { createProductController, deleteproductController, getproductsController, getsingleproductController, photoController, updateProductController } from '../controller/productController.js';
 import formidable from 'express-formidable';
 
 const router = express.Router();
 
 // create product route
 router.post('/create-product', requireSingIn, isAdmin, formidable(), createProductController);
+
+// update product route
+router.put('/update-product/:pid', requireSingIn, isAdmin, formidable(), updateProductController);
 
 // get all product route
 router.get('/get-product', getproductsController);
